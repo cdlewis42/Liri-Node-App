96327b7dce1d12648d217caeb1fb0d9e4d8c87b9
Celeste Lewis <cdlewis42@gmail.com>
2020-02-28 06:25:40 -0500

    Concert-This is working

--- liri.js
+++ liri.js
@@ -1,16 +1,37 @@
 require("dotenv").config();
 var keys = require("./keys.js")
-var spotify = new Spotify(keys.spotify)
+var axios = require("axios")
+var moment = require("moment")
+var fs = require("fs")
+//var spotify = new Spotify(keys.spotify)
+var nameOf = process.argv[3]
+showBands = function (bandName) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
+    axios.get(queryURL).then(
+        function (results) {
+            var data = results.data
+            for(var i=0; i<4; i++){
+            console.log(data[i].datetime)
+            console.log(data[i].venue.name)
+            console.log(data[i].venue.city)
+            console.log(data[i].venue.country)
+            console.log("")
+            }
+            
+        }
+    )
 
-if (process.argv[2] === "concert-this"){
-    showBands()
 }
-else if (process.argv[2] === "spotify-this-song"){
+var showMovies = function(){
+
+}
+
+if (process.argv[2] === "concert-this") {
+    showBands(nameOf)
+} else if (process.argv[2] === "spotify-this-song") {
     showSongs()
-}
-else if (process.argv[2] === "movie-this"){
+} else if (process.argv[2] === "movie-this") {
     showMovies()
-}
-else if (process.argv[2] === "do-what-it-says"){
+} else if (process.argv[2] === "do-what-it-says") {
     showRandom()
 }
