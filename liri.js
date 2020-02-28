589ca29336bc861581e9daa0c2401c9ddb22e744
Celeste Lewis <cdlewis42@gmail.com>
2020-02-28 06:52:12 -0500

    showMovies is working, but needs error check

--- liri.js
+++ liri.js
@@ -5,33 +5,59 @@ var moment = require("moment")
 var fs = require("fs")
 //var spotify = new Spotify(keys.spotify)
 var nameOf = process.argv[3]
+
+
+
+
 showBands = function (bandName) {
     var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
     axios.get(queryURL).then(
         function (results) {
             var data = results.data
-            for(var i=0; i<4; i++){
-            console.log(data[i].datetime)
-            console.log(data[i].venue.name)
-            console.log(data[i].venue.city)
-            console.log(data[i].venue.country)
-            console.log("")
+            for (var i = 0; i < 4; i++) {
+                console.log("Date: " + moment(data[i].datetime).format("MM/DD/YYYY"))
+                console.log("Venue: " + data[i].venue.name)
+                console.log("City: " + data[i].venue.city)
+                console.log("Country: " + data[i].venue.country)
+                console.log("")
             }
+
+        }
+    )
+
+}
+
+
+var showMovies = function (movieName) {
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy" 
+    axios.get(queryURL).then(
+        function (results) {
+            data = results.data
+                console.log("Title: " + data.Title)
+                console.log("Year released: " + data.Year)
+                console.log("IMDB rating: " + data.imdbRating)
+                console.log("Language: " + data.Language)
+                console.log("Plot: " + data.Plot)
+                console.log("Actors: " + data.Actors)
+
             
         }
     )
 
 }
-var showMovies = function(){
 
-}
 
 if (process.argv[2] === "concert-this") {
     showBands(nameOf)
 } else if (process.argv[2] === "spotify-this-song") {
-    showSongs()
+    showSongs(nameOf)
 } else if (process.argv[2] === "movie-this") {
-    showMovies()
+    if(nameOf === " "){
+        showMovies("Mr. Nobody")
+    }
+    else{
+    showMovies(nameOf)
+    }
 } else if (process.argv[2] === "do-what-it-says") {
-    showRandom()
+    showRandom(nameOf)
 }
